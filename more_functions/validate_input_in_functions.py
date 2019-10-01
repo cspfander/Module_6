77425d693d04bfa55b58188ec83ec5747378ec7f
packe <cspfander@dmacc.edu>
2019-09-30 23:18:22 -0500

    Initial Commit - All tests fail

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,24 @@
+"""
+Program: validate_input_in_functions.py
+Author: Colten Pfander
+Last date modified: 9/30/2019
+
+The purpose of this program is to write a function score_input() that takes a test_name, test_score, and
+invalid_message that validates the test_score, asking the user for a valid test score until it is in the range,
+then prints valid input as 'Test name: ##'.
+"""
+
+
+def score_input(test_name, test_score = 0, invalid_message = "Invalid test score, try again!"):
+    """
+    :param test_name: stores user input as a test name to print
+    :param test_score: stores a user input as a test score to print as well as be validated (optional)
+    :param invalid_message: optional message that displays an indicated string if the user has input an invalid score
+    :return: pass
+    """
+    # return {test_name: test_score}
+    pass
+
+
+if __name__ == '__main__':
+    score_input(24)
---
+++ test_functions/test_valid_input_in_functions.py
@@ -0,0 +1,26 @@
+import unittest
+from more_functions import validate_input_in_functions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual("Test name: 24", validate_input_in_functions.score_input(24))
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual("Test name: 12", validate_input_in_functions.score_input(12, 80))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual("Invalid test score, try again!", validate_input_in_functions.score_input((10, -1)))
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual("Invalid test score, try again!", validate_input_in_functions.score_input((9, 101)))
+
+    def test_score_non_numeric(self):
+        self.assertEqual("Invalid test score! Please use only numeric!", validate_input_in_functions.score_input(8, i))
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual("YOU MESSED UP!", validate_input_in_functions.score_input(7, -20, "YOU MESSED UP!"))
+
+
+if __name__ == '__main__':
+    unittest.main()
